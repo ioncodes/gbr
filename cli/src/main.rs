3b70efeac061446e22ebad5bd56ce356c8500e98
Luca Marcelli <ioncodes@users.noreply.github.com>
2017-06-23 18:21:35 +0200

    git

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,7 +3,7 @@ extern crate gbr;
 use gbr::cpu::CPU;
 
 fn main() {
-    let cpu = CPU::new("C:\\Users\\lucam\\Downloads\\Tetris.gb".to_owned());
+    let cpu = CPU::new("../roms/tetris.gb".to_owned());
     cpu.cycle();
     println!("Hello, world!");
 }
